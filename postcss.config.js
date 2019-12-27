c31c9cfa734c971adb5163ddb5fc8439a68b79be
Rahman Younus <rahman_95@live.co.uk>
2019-12-27 10:33:39 +0000

    Initial commit

---
+++ postcss.config.js
@@ -0,0 +1,16 @@
+const tailwindcss = require("tailwindcss");
+
+const purgecss = require("@fullhuman/postcss-purgecss")({
+  content: ["./src/**/*.jsx", "./src/**/*.js", "./public/index.html"],
+  css: ["./src/styles/tailwind.css"],
+  // Include any special characters you're using in this regular expression
+  defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || []
+});
+
+module.exports = {
+  plugins: [
+    tailwindcss("./tailwind.js"),
+    require("autoprefixer"),
+    ...(process.env.NODE_ENV === "production" ? [purgecss] : [])
+  ]
+};
